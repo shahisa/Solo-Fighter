11bdd22465757b08b58b01888f1602c6096298bf
Isaiah Weaver <iswea1@morgan.edu>
2016-05-08 02:30:23 -0400

    added explosions

--- Solo Fighter/GameScene.swift
+++ Solo Fighter/GameScene.swift
@@ -9,11 +9,23 @@
 import SpriteKit
 import AVFoundation
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     let player = SKSpriteNode(imageNamed: "playerShip")
     let bulletSound = SKAction.playSoundFileNamed("laserSound.mp3", waitForCompletion: false)
-   
+    struct PhysicsCategories {
+        //Need explaination of why this works and uses of struct
+        /*
+        1. A struct should represent a single value.
+        2. A struct should have a memory footprint less than 16 bytes.
+        3. A struct should not be changed after creation.
+        */
+        static let None: UInt32 = 0
+        static let Player: UInt32 = 0b1 //1
+        static let Bullet: UInt32 = 0b10 //2
+        static let Enemy: UInt32 = 0b100 //4
+        
+    }
     
     
    //functions are for randomly generating enemyShips
@@ -26,6 +38,8 @@ class GameScene: SKScene {
     func random(min min:CGFloat, max: CGFloat) -> CGFloat {
         return random() * (max - min) + min
     }
+    
+    
     
     //This is to limit the game area size, need the ship not to leave the screen
     
@@ -49,7 +63,8 @@ class GameScene: SKScene {
     
     override func didMoveToView(view: SKView) {
         
-        
+        //Physics Body
+        self.physicsWorld.contactDelegate = self
         //Background Imagae
         let background = SKSpriteNode(imageNamed: "background")
         background.size = self.size
@@ -62,6 +77,11 @@ class GameScene: SKScene {
         player.setScale(1)
         player.position = CGPoint(x: self.size.width/2, y: self.size.height * 0.2)
         player.zPosition = 2
+        player.physicsBody = SKPhysicsBody(rectangleOfSize: player.size)
+        player.physicsBody!.affectedByGravity = false
+        player.physicsBody!.categoryBitMask = PhysicsCategories.Player
+        player.physicsBody!.collisionBitMask = PhysicsCategories.None
+        player.physicsBody!.contactTestBitMask = PhysicsCategories.Enemy
         self.addChild(player)
   
         // Background Music
@@ -79,6 +99,58 @@ class GameScene: SKScene {
         
     }
     
+    //Will explain later
+    func didBeginContact(contact: SKPhysicsContact) {
+        var body1 = SKPhysicsBody()
+        var body2 = SKPhysicsBody()
+        
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask{
+            body1 = contact.bodyA
+            body2 = contact.bodyB
+            
+        }else{
+            body1 = contact.bodyB
+            body2 = contact.bodyA
+        }
+        if body1.categoryBitMask == PhysicsCategories.Player && body2.categoryBitMask == PhysicsCategories.Enemy{
+            if body1.node != nil { //adding this so the game doesn't crash (this code doesn't look very good to me, might be a better way to do this)
+            spawnExplosion(body1.node!.position)
+            }
+            if body2.node != nil {
+            spawnExplosion(body2.node!.position) //same reasoning as body1.node != nill if statement
+            }
+            //if the player has hit the enemy
+            body1.node?.removeFromParent() // its because node is an optional on this line. And we unwrapped it above
+            body2.node?.removeFromParent()
+        }
+        if body1.categoryBitMask == PhysicsCategories.Bullet && body2.categoryBitMask == PhysicsCategories.Enemy && body2.node?.position.y < self.size.height{
+            
+            if body2.node != nil {
+            spawnExplosion(body2.node!.position)
+            }
+            //if the bullet has hit the enemy
+            body1.node?.removeFromParent()
+            body2.node?.removeFromParent()
+        }
+    }
+    
+    func spawnExplosion(spawnPosition: CGPoint) {
+        //Create explosion
+        let explosion = SKSpriteNode(imageNamed: "explosion")
+        explosion.position = spawnPosition
+        explosion.zPosition = 3
+        explosion.setScale(0)
+        self.addChild(explosion)
+        
+        let scaleIn = SKAction.scaleTo(1, duration: 0.1)
+        let fadeOut = SKAction.fadeOutWithDuration(0.1)
+        let delete = SKAction.removeFromParent()
+        
+        
+        let explosionSequence = SKAction.sequence([scaleIn, fadeOut, delete])
+        explosion.runAction(explosionSequence)
+    }
+    
     
     func startNewLevel(){
         
@@ -92,10 +164,16 @@ class GameScene: SKScene {
     }
 
     func fireBullet (){
+        // make bullet
         let bullet = SKSpriteNode(imageNamed: "bullet")
         bullet.setScale(1)
         bullet.position = player.position
         bullet.zPosition = 1
+        bullet.physicsBody = SKPhysicsBody(rectangleOfSize: bullet.size)
+        bullet.physicsBody!.affectedByGravity = false
+        bullet.physicsBody!.categoryBitMask = PhysicsCategories.Bullet
+        bullet.physicsBody!.collisionBitMask = PhysicsCategories.None
+        bullet.physicsBody!.contactTestBitMask = PhysicsCategories.Enemy
         self.addChild(bullet)
         
         let moveBullet = SKAction.moveToY(self.size.height + bullet.size.height, duration: 1)
@@ -112,11 +190,16 @@ class GameScene: SKScene {
         
         let startPoint = CGPoint(x: randomXStart, y: self.size.height * 1.2)
         let endPoint = CGPoint(x: randomXEnd, y: -self.size.height * 0.2)
-        
+        //Creation of enemy
         let enemy = SKSpriteNode(imageNamed: "enemyShip")
         enemy.setScale(1)
         enemy.position = startPoint
         enemy.zPosition = 2
+        enemy.physicsBody = SKPhysicsBody(rectangleOfSize: enemy.size)
+        enemy.physicsBody!.affectedByGravity = false
+        enemy.physicsBody!.categoryBitMask = PhysicsCategories.Enemy
+        enemy.physicsBody!.collisionBitMask = PhysicsCategories.None
+        enemy.physicsBody!.contactTestBitMask = PhysicsCategories.Player | PhysicsCategories.Bullet
         self.addChild(enemy)
         
         let moveEnemy = SKAction.moveTo(endPoint, duration: 1.5)
